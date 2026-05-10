69e798474e5efd5b6a9fdd12d08538f018df124a
Kachimov Dmytro <romanov.dima.13.11.98@gmail.com>
2026-05-10 17:55:39 +0300

    prepare php app for railway

--- core/Database.php
+++ core/Database.php
@@ -1,3 +1,19 @@
 <?php
+
+$host = getenv('MYSQLHOST');
+$port = getenv('MYSQLPORT');
+$db   = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+
 require VENDOR . '/rb.php';
-R::setup( 'mysql:host=localhost;dbname=to_doo', 'root', 'root' );
+
+R::setup(
+    "mysql:host={$host};port={$port};dbname={$db};charset=utf8mb4",
+    $user,
+    $pass
+);
+
+if (!R::testConnection()) {
+    die('Database connection failed');
+}
